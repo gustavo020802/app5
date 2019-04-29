06cd21fba33671bd2ac10c3e3a9057b6c358acf4
Gustavo <gusben13@gmail.com>
2019-04-29 14:17:25 -0500

    3

--- js/acciones.js
+++ js/acciones.js
@@ -1,3 +1,17 @@
 // JavaScript Document
 
+$(document).read(function(e){
+document.addEventListener("deviceready", function(){
+	
+$('#beep').tap(function(){
+navigator.notification.beep(5);
+});//tap beep
+
+$('#vibrar').tap(function(){
+navigator.notification.vibrate(5000);
+});//tap vibrar
+
+},false);//deviceready
+}); //ready
+
 
